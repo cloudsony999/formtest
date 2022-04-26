fdb6c6d7e6771fa2a22e57fe87d9e8a419543b9c
cloudsony999 <cloudsony999@gmail.com>
2022-04-26 14:57:03 +0530

    form commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formtest';
-}
+  
+  uname=new FormControl('');
+  updateName()
+  {
+    this.uname.setValue("AMITAVA");
+  }
+  person1:number=0;
+  person2:number=0;
+  tall=false;
+  ph1=new FormControl(0);
+  ph2=new FormControl(0);
+
+  findTaller()
+  {
+    this.person1=Number(this.ph1.value);
+    this.person2=Number(this.ph2.value);
+    if (this.person1>this.person2)
+    {
+    this.tall=true;
+    }
+    else
+    {
+    this.tall=false;
+    }
+  }
+    products=[
+      {name:"Mobile",price:30000,company:"Samsung"},
+      {name:"Music System",price:34000,company:"JBL"},
+      {name:"AC",price:38000,company:"Hitachi"}
+      
+    ];
+  }
+  
+
